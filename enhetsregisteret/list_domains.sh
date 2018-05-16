671b084171129f5746aa2f84343cbb3091a02f4a
Anders Einar Hilden <hildenae@gmail.com>
2018-05-16 22:03:09 +0000

    Better messages, use old list as input for new list

--- enhetsregisteret/list_domains.sh
+++ enhetsregisteret/list_domains.sh
@@ -2,23 +2,24 @@
 # Info: http://data.norge.no/data/registerenheten-i-br%C3%B8nn%C3%B8ysund/enhetsregisteret
 # Download: http://hotell.difi.no/download/brreg/enhetsregisteret?download
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${SOURCE_DIR}"
+cd "$SOURCE_DIR"
 source config.sh # MCN_TOOLS, DOMAINS
 
 # Timestamping is turned off on the server, so we can't use it for wget
 # Download if missing or --update-source
 if [ ! -e 'enhetsregisteret.csv' ] || [ "x$1" = "x--update-src" ]; then
-    echo "INFO: enhetsregisteret.csv was not found, downloading from hotell.difi.no" 1>&2
+    echo "[INFO] enhetsregisteret.csv was not found, downloading from hotell.difi.no" 1>&2
     wget -O enhetsregisteret.csv "http://hotell.difi.no/download/brreg/enhetsregisteret?download"
 else
-    echo "INFO: enhetsregisteret.csv found. Use '$0 ----update-src' to redownload." 1>&2
+    echo "[INFO] enhetsregisteret.csv found. Use '$0 ----update-src' to redownload." 1>&2
 fi
 
-if [ ! -e "${DOMAINS}" ] || [ "x$1" = "x--update-list" ]; then
-    echo "INFO: ${DOMAINS} not found, extracting ..." 1>&2
-    cat ./enhetsregisteret.csv | ${MCN_TOOLS}/default_extract > "${DOMAINS}"
+if [ ! -e "$DOMAINS" ] || [ "x$1" = "x--update-list" ]; then
+    echo "[INFO] $DOMAINS not found, extracting ..." 1>&2
+    cat ./enhetsregisteret.csv "$DOMAINS" 2>/dev/null | ${MCN_TOOLS}/default_extract > "_tmp.list"
+    mv "_tmp.list" "$DOMAINS"
+    rm -f "_tmp.list" 
 else
-    echo "INFO: ${DOMAINS} found. Use '$0 --update-list' to force new extraction." 1>&2
+    echo "[INFO] $DOMAINS found. Use '$0 --update-list' to force new extraction." 1>&2
 fi
-cat "${DOMAINS}"
 
