45a1ea7f87a1e197891c89ae372578f87a3d3c50
Anders Einar Hilden <hildenae@gmail.com>
2019-01-17 13:05:05 +0100

    Fix age checks and use output folder

--- bedriftshelsetjenester/list_domains.sh
+++ bedriftshelsetjenester/list_domains.sh
@@ -6,18 +6,21 @@ cd "$SOURCE_DIR"
 source config.sh # MCN_TOOLS, DOMAINS
 
 INPUT="bedriftshelsetjeneste.xml" # Updates daily: -mtime 1
-if [ ! -e "$INPUT" ] || [ "x$1" = "x--update-src" ] || [ "`find "$INPUT" -mtime 1`" ]; then
+if [ ! -e "$INPUT" ] || [ "x$1" = "x--update-src" ] || find "$INPUT" ! -mtime -1 | egrep '.*' >/dev/null; then
     echo "INFO: $INPUT was not found or older than X, downloading from arbeidstilsynet.no" 1>&2
     wget -O "$INPUT" "http://www.arbeidstilsynet.no/opendata/bedriftshelsetjeneste.xml"
 else
     echo "INFO: $INPUT found. Use '$0 --update-src' to redownload." 1>&2
 fi
 
-if [ ! -e "$DOMAINS" ] || [ "x$1" = "x--update-list" ] || [ "`find $INPUT ! -mmin +1`"  ]; then
-    echo "INFO: $DOMAINS not found or $INPUT is fresh, extracting ..." 1>&2
-    cat "$INPUT" "$DOMAINS" 2>/dev/null | "$MCN_TOOLS/default_extract" > "_tmp.list"
-    mv "_tmp.list" "$DOMAINS"
+YMD="$(date +%F)"
+
+if [ ! -e "output/$YMD-$DOMAINS" ] || [ "x$1" = "x--update-list" ] || find "$INPUT" -mmin -1 | egrep '.*' >/dev/null; then
+    echo "INFO: output/$YMD-$DOMAINS not found or $INPUT is modified in last minute, extracting ..." 1>&2
+    mv output/* old/
+    cat "$INPUT" old/*.list 2>/dev/null | "$MCN_TOOLS/default_extract" > "_tmp.list"
+    mv "_tmp.list" "output/$YMD-$DOMAINS"
 else
-    echo "INFO: $DOMAINS found. Use '$0 --update-list' to force new extraction." 1>&2
+    echo "INFO: output/$YMD-$DOMAINS found. Use '$0 --update-list' to force new extraction." 1>&2
 fi
 
