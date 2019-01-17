f7682e3081350cd612c25eb966505b02909f8ba2
Anders Einar Hilden <hildenae@gmail.com>
2019-01-17 14:19:48 +0100

    enhetsreg: use output fodlers, reuse old data, check for new file after two days

--- enhetsregisteret/list_domains.sh
+++ enhetsregisteret/list_domains.sh
@@ -5,21 +5,23 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SOURCE_DIR"
 source config.sh # MCN_TOOLS, DOMAINS
 
+INPUT="enhetsregisteret.csv"
+YMD="$(date +%F)"
 # Timestamping is turned off on the server, so we can't use it for wget
 # Download if missing or --update-source
-if [ ! -e 'enhetsregisteret.csv' ] || [ "x$1" = "x--update-src" ]; then
-    echo "[INFO] enhetsregisteret.csv was not found, downloading from hotell.difi.no" 1>&2
-    wget -O enhetsregisteret.csv "http://hotell.difi.no/download/brreg/enhetsregisteret?download"
+if [ ! -e "$INPUT" ] || [ "x$1" = "x--update-src" ] || find "$INPUT" ! -mtime -1 | egrep '.*' >/dev/null; then
+    echo "[INFO] $INPUT was not found, downloading from hotell.difi.no" 1>&2
+    wget -O "$INPUT" "http://hotell.difi.no/download/brreg/enhetsregisteret?download"
 else
-    echo "[INFO] enhetsregisteret.csv found. Use '$0 ----update-src' to redownload." 1>&2
+    echo "[INFO] $INPUT found. Use '$0 ----update-src' to redownload." 1>&2
 fi
 
-if [ ! -e "$DOMAINS" ] || [ "x$1" = "x--update-list" ]; then
-    echo "[INFO] $DOMAINS not found, extracting ..." 1>&2
-    cat ./enhetsregisteret.csv "$DOMAINS" 2>/dev/null | ${MCN_TOOLS}/default_extract > "_tmp.list"
-    mv "_tmp.list" "$DOMAINS"
-    rm -f "_tmp.list" 
+if [ ! -e "$YMD-$DOMAINS" ] || [ "x$1" = "x--update-list" ]; then
+    echo "[INFO] $YMD-$DOMAINS not found, extracting ..." 1>&2
+    mv output/* old/
+    cat ./$INPUT old/* 2>/dev/null | ${MCN_TOOLS}/default_extract > "_tmp.list"
+    mv "_tmp.list" "output/$YMD-$DOMAINS"
 else
-    echo "[INFO] $DOMAINS found. Use '$0 --update-list' to force new extraction." 1>&2
+    echo "[INFO] output/$YMD-$DOMAINS found. Use '$0 --update-list' to force new extraction." 1>&2
 fi
 
